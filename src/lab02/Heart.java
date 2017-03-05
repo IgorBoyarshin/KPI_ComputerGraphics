e2c8fd8ff1443bab5a493ed4c58370de2046554c
IgorBoyarshin <igorb314@gmail.com>
2017-03-05 16:39:05 +0200

    Lab02. Dynamic stuff added

---
+++ src/lab02/Heart.java
@@ -0,0 +1,87 @@
+package lab02;
+
+import math.Curve;
+import math.Vector2d;
+
+import java.util.function.Function;
+
+/**
+ * Created by Igorek on 05-Mar-17 at 2:07 PM.
+ */
+public class Heart extends Curve {
+
+    private double R0;
+    private double R1;
+    private double R2;
+    private double R3;
+    private double R4;
+
+    public Heart(double R0, double R1, double R2, double R3, double R4, double canvasSize) {
+        super(
+                t -> funcXOfT(t, R0),
+                t -> funcYOfT(t, R1, R2, R3, R4),
+                new Vector2d(0.0, 2.0 * Math.PI),
+                0.1,
+                canvasSize
+        );
+
+        this.R0 = R0;
+        this.R1 = R1;
+        this.R2 = R2;
+        this.R3 = R3;
+        this.R4 = R4;
+    }
+
+    private static double funcXOfT(double t, double R0) {
+        final double sin = Math.sin(t);
+        return R0 * sin * sin * sin;
+    }
+
+    private static double funcYOfT(double t, double R1, double R2, double R3, double R4) {
+        return (R1 * Math.cos(1 * t) - R2 * Math.cos(2 * t) - R3 * Math.cos(3 * t) - R4 * Math.cos(4 * t));
+    }
+
+    private void updateFunctionXOfT() {
+        this.setXOfT(t -> funcXOfT(t, R0));
+    }
+
+    private void updateFunctionYOfT() {
+        this.setYOfT(t -> funcYOfT(t, R1, R2, R3, R4));
+    }
+
+    public void setRs(double newR0, double newR1, double newR2, double newR3, double newR4) {
+        this.R0 = newR0;
+        this.R1 = newR1;
+        this.R2 = newR2;
+        this.R3 = newR3;
+        this.R4 = newR4;
+
+        this.updateFunctionXOfT();
+        this.updateFunctionYOfT();
+    }
+
+    public void setR0(double newR) {
+        this.R0 = newR;
+        this.updateFunctionXOfT();
+    }
+
+    public void setR1(double newR) {
+        this.R1 = newR;
+        this.updateFunctionYOfT();
+    }
+
+    public void setR2(double newR) {
+        this.R2 = newR;
+        this.updateFunctionYOfT();
+    }
+
+    public void setR3(double newR) {
+        this.R3 = newR;
+        this.updateFunctionYOfT();
+    }
+
+    public void setR4(double newR) {
+        this.R4 = newR;
+        this.updateFunctionYOfT();
+    }
+}
--- src/lab02/Lab02.java
+++ src/lab02/Lab02.java
@@ -6,14 +6,9 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import math.Curve;
 import math.Vector2d;
-
-import java.util.function.DoubleSupplier;
-import java.util.function.Function;
 
 public class Lab02 extends Application {
     private Stage window;
@@ -21,8 +16,20 @@ public class Lab02 extends Application {
 
     private final double canvasSize = 500.0;
 
-    private final Curve element = new Curve(t -> 50 * Math.cos(t), t -> 50 * Math.sin(t),
-            new Vector2d(0.0, 2 * Math.PI), 0.1, canvasSize);
+    private final Heart heart;
+
+    final double heartSize = 100.0;
+
+    public Lab02() {
+        heart = new Heart(
+                getR0(heartSize),
+                getR1(heartSize),
+                getR2(heartSize),
+                getR3(heartSize),
+                getR4(heartSize),
+                canvasSize
+        );
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -55,13 +62,87 @@ public class Lab02 extends Application {
         window.show();
     }
 
-
     private void draw(GraphicsContext gc, double time) {
-        drawElement(gc, new Vector2d(canvasSize / 2.0, canvasSize / 2.0), Color.RED);
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, canvasSize, canvasSize);
+
+        // #1
+        for (double size = heartSize; size >= heartSize / 24.0; size -= heartSize / 12.0) {
+            final double baseR0 = getR0(size);
+            final double baseR4 = getR4(size);
+            heart.setRs(
+                    baseR0 + baseR0 / 3.0 * Math.sin(2 * time),
+                    getR1(size),
+                    getR2(size),
+                    getR3(size),
+                    baseR4 + baseR4 * 0.75 * Math.sin(2 * time)
+            );
+
+            heart.draw(gc, new Vector2d(canvasSize / 4.0, canvasSize / 2.0), 0.0,
+                    Color.rgb((int) (size / heartSize * 255), (int) (0.2 * 255), (int) (0.4 * 255)));
+        }
+
+        // #2 first circle
+        heart.setRs(
+                getR0(heartSize),
+                getR1(heartSize),
+                getR2(heartSize),
+                getR3(heartSize),
+                getR4(heartSize)
+        );
+        for (double angle = 0.0; angle < 360; angle += 30.0) {
+            final double shiftedAngle = angle + (time * 60) % 30;
+
+            heart.draw(gc, new Vector2d(canvasSize * 0.75, canvasSize / 2.0), shiftedAngle, Color.BLUE);
+        }
+
+        // #2 second circle
+        heart.setRs(
+                getR0(heartSize / 2.0),
+                getR1(heartSize / 2.0),
+                getR2(heartSize / 2.0),
+                getR3(heartSize / 2.0),
+                getR4(heartSize / 2.0)
+        );
+        for (double angle = 0.0; angle < 360; angle += 30.0) {
+            final double shiftedAngle = angle + (time * 60) % 30;
+
+            heart.draw(gc, new Vector2d(canvasSize * 0.75, canvasSize / 2.0), shiftedAngle, Color.GREEN);
+        }
+
+        // #2 third circle
+        heart.setRs(
+                getR0(heartSize / 4.0),
+                getR1(heartSize / 4.0),
+                getR2(heartSize / 4.0),
+                getR3(heartSize / 4.0),
+                getR4(heartSize / 4.0)
+        );
+        for (double angle = 0.0; angle < 360; angle += 30.0) {
+            final double shiftedAngle = angle + (time * 30) % 30;
+
+            heart.draw(gc, new Vector2d(canvasSize * 0.75, canvasSize / 2.0), shiftedAngle, Color.RED);
+        }
     }
 
-    private void drawElement(GraphicsContext gc, Vector2d center, Color color) {
-        element.draw(gc, center, color);
+    private double getR0(double heartSize) {
+        return heartSize * 0.7;
+    }
+
+    private double getR1(double heartSize) {
+        return heartSize / 3.0 * 2.0;
+    }
+
+    private double getR2(double heartSize) {
+        return heartSize / 6.0;
+    }
+
+    private double getR3(double heartSize) {
+        return heartSize / 6.0;
+    }
+
+    private double getR4(double heartSize) {
+        return heartSize / 12.0;
     }
 
     public static void main(String[] args) {
